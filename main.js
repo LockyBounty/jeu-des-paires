af7e20811d839784c3983846eeab2f49494e7f1b
Locky <inox127@hotmail.com>
2018-10-24 12:23:43 +0200

    added lines

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 let objetImages = {};
-let tableauCartes = ["images/anehihan.jpg","images/chatminou.jpg","images/chientoutou.jpg","images/lamacrachat.jpg","images/lapinscrottes.jpg","images/lionnegraou.jpg","images/oursbaby.jpg"];
-let setImg = [];
-for (i=0;i<tableauCartes;i++){
-    setImg = document.querySelector(`#card-${i}`).src = tableauCartes[i];
-    console.log(setImg);
-}
-let a = document.querySelector("#card-1");
+let tableauCartes = ["images/anehihan.jpg", "images/chatminou.jpg", "images/chientoutou.jpg", "images/lamacrachat.jpg", "images/lapinscrottes.jpg", "images/lionnegraou.jpg", "images/oursbaby.jpg"];
+let tableauCartesclone = tableauCartes;
+let tableauConcatenation = tableauCartes.concat(tableauCartesclone);
+let imgCache = "images/what1.png";
+
+let clickToWatch = document.querySelector("[id*='card']");
+let deviner = [];
+let cpt = true;
+console.log(tableauConcatenation);
+
+let allImg = document.querySelectorAll("img");
+
 let attribution = () => {
+    for (i = 0; i < tableauConcatenation.length; i++) {
+        allImg[i].src = `${tableauConcatenation[Math.floor((i)*1)]}`;
 
+    }
+}
 
-// for (i=0; i<tableauCartes.length; i++){
-//    a[i].src = `"${tableauCartes[i]}"`;
-// }
+let cacheStart = () => {
 
-// console.log(i);
+    for (i = 0; i < tableauConcatenation.length; i++) {
+        allImg[i].src = `${imgCache}`;
+    }
+}
 
-// }
+let unVeil = () => {
+    if (cpt === true) {
 
-a.addEventListener("click", attribution);
+        cpt = false;
+        return this;
+    }
+
+else {
+    for (i = 0; i < tableauConcatenation.length; i++) {
+        cacheStart();
+        cpt = true;
+    }
+}
+}
+
+/*Cacher toutes les images au depart */
+cacheStart();
+attribution();
+clickToWatch.addEventListener("click", attribution);
